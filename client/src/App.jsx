5af65e061397518c5791915a584803babbf47731
Dave <justindave2828@gmail.com>
2025-05-09 15:47:09 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Home from './pages/Home';
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+  );
+}
+
+export default App
