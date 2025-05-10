969df67b09d92c927a34773fbeb072ed61b7fb8a
DarwinJohan <darwinjohan001@gmail.com>
2025-05-10 19:19:56 +0700

    Penambahan Product List
    Penambahan Product List

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,31 +1,27 @@
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
-import './Dashboard.css';
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import ProductGrid from "../components/ProductGrid";
+import "./Dashboard.css";
 
 function Dashboard() {
+  return (
+    <div className="app-layout">
+      <Navbar />
 
+      <div className="main-content">
+        <>
+          <Sidebar />
+        </>
 
-    return (
-        <div className="app-layout">
-
-            <Navbar />
-
-
-            <div className="main-content">
-                
-                <>
-                    <Sidebar />
-                </>
-                
-                <div className="page-content">
-                    <div>
-                        Content
-                    </div>
-                </div>
-                
-            </div>
+        <div className="page-content">
+          <div>Content</div>
+          <div className="page-content">
+            <ProductGrid itemCount={50} />{" "}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
